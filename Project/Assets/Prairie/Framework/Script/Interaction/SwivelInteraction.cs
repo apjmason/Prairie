1c3c150a08d85ef614afbf3c39e98f7f03ba3a9f
Matt <javalym@carleton.edu>
2016-11-01 01:13:45 -0500

    Door swivels about 90 degrees upon interaction.

---
+++ Project/Assets/Prairie/Framework/Script/Interaction/SwivelInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwivelInteraction : Interaction {
+
+	public GameObject hinge;
+	private bool open = false;
+	private float rotateTime;
+	
+	// Update is called once per frame
+	protected void Update()
+	{
+		if (open)
+		{
+			transform.RotateAround(hinge.transform.position, new Vector3 (0,1,0), 200*Time.deltaTime);
+			rotateTime += 1;
+			if (rotateTime == 22) {
+				rotateTime = 0;
+				open = false;
+			}
+		}
+	}
+
+	protected override void PerformAction() {
+		open = !open;
+	}
+}
