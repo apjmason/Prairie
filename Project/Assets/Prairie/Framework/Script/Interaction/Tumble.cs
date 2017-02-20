803c1c3059d6986aa899de99b3f60561ebe7da23
Matt <javalym@carleton.edu>
2017-02-20 13:55:42 -0600

    Object now moves to be a fixed distance from player when picked up, and snaps back to its original rotation/position after being put down.

--- Project/Assets/Prairie/Framework/Script/Interaction/Tumble.cs
+++ Project/Assets/Prairie/Framework/Script/Interaction/Tumble.cs
@@ -7,11 +7,23 @@ public class Tumble : PromptInteraction
 	/// <summary>
 	/// Allows user to rotate object.
 	/// </summary>
-	private bool pickedUp = false;
+	private bool pickedUp;
+	private Quaternion oldRotation;
+	private Vector3 oldPosition;
+	private int distance;
+	private Ray hit;
 
 	// When the user interacts with object, they invoke the ability to 
 	// tumble the object with the I, J, K and L keys. Interacting
 	// with the object again revokes this ability.
+
+	void Start()
+	{
+		pickedUp = false;
+		oldRotation = Quaternion.identity;
+		oldPosition = this.transform.position;
+		distance = 1;
+	}
 
 	protected void Update()
 	{
@@ -33,6 +45,10 @@ public class Tumble : PromptInteraction
 			{
 				transform.RotateRelativeToCamera (0, -10);
 			}
+			else if (Input.GetKey (KeyCode.Escape))
+			{
+				this.PerformAction();
+			}
 		}
 	}
 
@@ -42,11 +58,15 @@ public class Tumble : PromptInteraction
 		if (player != null) {
 			if (pickedUp)
 			{
+				hit = new Ray(transform.position, Camera.main.transform.position - transform.position);
+				this.transform.position = hit.GetPoint(distance);
 				player.SetCanMove (false);
 				player.SetDrawsGUI (false);
 			}
 			else
 			{
+				this.transform.rotation = oldRotation;
+				this.transform.position = oldPosition;
 				player.SetCanMove (true);
 				player.SetDrawsGUI (true);
 			}
