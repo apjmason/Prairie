4eef60770e9776a908d5cf2047b15c2daa6a2e7f
Huiji (Chelsea) Ying <yingh@carleton.edu>
2017-11-02 10:19:49 -0500

    Add Preliminary Inventory system.

---
+++ Project/Assets/Editor/CustomInspectors/InventoryEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(Inventory))]
+public class InventoryEditor : Editor
+{
+	private bool[] showItemSlots = new bool[Inventory.numSlots];
+	private SerializedProperty objNameProperty;
+	private SerializedProperty objsProperty;
+	private const string inventoryPropobjNamesName = "objImages";
+	private const string inventoryPropobjsName = "objs";
+	private void OnEnable ()
+	{
+		objNameProperty = serializedObject.FindProperty (inventoryPropobjNamesName);
+		objsProperty = serializedObject.FindProperty (inventoryPropobjsName);
+	}
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.Update ();
+		for (int i = 0; i < Inventory.numSlots; i++)
+		{
+			ItemSlotGUI (i);
+		}
+		serializedObject.ApplyModifiedProperties ();
+	}
+	private void ItemSlotGUI (int index)
+	{
+		EditorGUILayout.BeginVertical (GUI.skin.box);
+		EditorGUI.indentLevel++;
+
+		showItemSlots[index] = EditorGUILayout.Foldout (showItemSlots[index], "Item slot " + index);
+		if (showItemSlots[index])
+		{
+			if (objNameProperty == null) {
+				Debug.Log ("objNameProperty is null");
+			}
+			EditorGUILayout.PropertyField (objNameProperty.GetArrayElementAtIndex (index));
+			EditorGUILayout.PropertyField (objsProperty.GetArrayElementAtIndex (index));
+		}
+		EditorGUI.indentLevel--;
+		EditorGUILayout.EndVertical ();
+	}
+}
---
+++ Project/Assets/Prairie/Framework/Script/Interaction/Inventory.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Inventory : MonoBehaviour
+{	
+	public const int numSlots = 4;
+	public Image[] objImages = new Image[numSlots];
+	public GameObject[] objs = new GameObject[numSlots];
+
+//	int rows = 2;
+//	int columns = 5;
+//
+//	private GameObject player;
+
+//	// Use this for initialization
+//	void Start ()
+//	{
+////		// Need to attach this script to 
+////		player = this.gameObject;
+////		Debug.Log ("Inventory attached to " + player.name);
+//	}
+
+	public void AddToInventory (GameObject objToAdd)
+	{
+		for (int i = 0; i < objs.Length; i++) {
+			if (objs [i] == null) {
+				objs [i] = objToAdd;
+				objImages [i] = objToAdd.GetComponents<Image>()[0];
+				objImages [i].enabled = true;
+				return;
+			}
+		}
+	}
+
+	public void RemoveFromInventory (GameObject objToRemove)
+	{
+		for (int i = 0; i < objs.Length; i++) {
+			if (objs [i] == objToRemove) {
+				objs [i] = null;
+				objImages [i] = null;
+				objImages [i].enabled = false;
+				return;
+			}
+		}
+	}
+}
+
