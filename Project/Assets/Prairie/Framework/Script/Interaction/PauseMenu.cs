75205b7c77a2ca85f09e272d2675ea9dbfd09a18
Charlieranderson <Charlieranderson@gmail.com>
2018-01-13 16:08:25 -0600

    Adding just the start menu updates - new navigation options

--- Project/Assets/Prairie/Framework/Script/Interaction/PauseMenu.cs
+++ Project/Assets/Prairie/Framework/Script/Interaction/PauseMenu.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
 
+	private bool open;
+
 	// Use this for initialization
 	void Awake () {
+		open = false;
 		gameObject.transform.GetChild(0).gameObject.SetActive (false);
+		gameObject.transform.GetChild(1).gameObject.SetActive (false);
+		gameObject.transform.GetChild(2).gameObject.SetActive (false);
 		Debug.Log (gameObject.transform.GetChild (0).gameObject.activeSelf);
+		Debug.Log (open);
 	}
 	
 	void Update(){
 		if (Input.GetKeyDown ("escape")) {
-			if (gameObject.transform.GetChild(0).gameObject.activeSelf == false) {
+			//if (gameObject.transform.GetChild(0).gameObject.activeSelf == false) {
+			if (!open) {
 				gameObject.transform.GetChild(0).gameObject.SetActive (true);
+				open = true;
 				Time.timeScale = 0;
-				Debug.Log("Pressed esc, paused");
 				Cursor.visible = true;
+				Debug.Log (open);
 				GameObject.FindGameObjectsWithTag ("Player")[0].GetComponent<FirstPersonInteractor> ().enabled = false;
 			} else {
+				open = false;
 				gameObject.transform.GetChild(0).gameObject.SetActive (false);
+				gameObject.transform.GetChild(1).gameObject.SetActive (false);
+				gameObject.transform.GetChild(2).gameObject.SetActive (false);
 				Time.timeScale = 1;
 				Cursor.visible = false;
-				//Debug.Log("Pressed esc, unpaused");
 				GameObject.FindGameObjectsWithTag ("Player")[0].GetComponent<FirstPersonInteractor> ().enabled = true;
 
 			}
@@ -39,10 +49,25 @@ public class PauseMenu : MonoBehaviour {
 
 	}
 
+	public void showKeybindings(){
+		gameObject.transform.GetChild(0).gameObject.SetActive (false);
+		gameObject.transform.GetChild(2).gameObject.SetActive (true);
+	}
+
+	public void options(){
+		gameObject.transform.GetChild(0).gameObject.SetActive (false);
+		gameObject.transform.GetChild(1).gameObject.SetActive (true);
+	}
 
 	public void quit(){
 		Debug.Log ("is this quitting?");
 		Application.Quit();
+	}
+
+	public void backToMainMenu(){
+		gameObject.transform.GetChild(0).gameObject.SetActive (true);
+		gameObject.transform.GetChild(1).gameObject.SetActive (false);
+		gameObject.transform.GetChild(2).gameObject.SetActive (false);
 	}
 
 }
