3351051e41a4083d88ce8ef2eaeaab043109b563
Charlieranderson <Charlieranderson@gmail.com>
2018-01-15 23:15:58 -0600

    Merged inventory UI and Start menu ui, began creation of journal, made significant updates to the menu code so heirarchy order is thankfully not considered. All UI objects are now on the prairie player prefab. UI is also now scalable.

--- Project/Assets/Prairie/Framework/Script/Interaction/PauseMenu.cs
+++ Project/Assets/Prairie/Framework/Script/Interaction/PauseMenu.cs
@@ -5,35 +5,28 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour {
 
 	private bool open;
+	private bool showInventoryUI;
 
 	// Use this for initialization
 	void Awake () {
 		open = false;
-		gameObject.transform.GetChild(0).gameObject.SetActive (false);
-		gameObject.transform.GetChild(1).gameObject.SetActive (false);
-		gameObject.transform.GetChild(2).gameObject.SetActive (false);
-		Debug.Log (gameObject.transform.GetChild (0).gameObject.activeSelf);
-		Debug.Log (open);
+		showInventoryUI = true;
+		gameObject.transform.Find("Pause Menu").gameObject.SetActive (false);
+		gameObject.transform.Find("Options Menu").gameObject.SetActive (false);
+		gameObject.transform.Find("Keybindings").gameObject.SetActive (false);
 	}
 	
 	void Update(){
 		if (Input.GetKeyDown ("escape")) {
 			//if (gameObject.transform.GetChild(0).gameObject.activeSelf == false) {
 			if (!open) {
-				gameObject.transform.GetChild(0).gameObject.SetActive (true);
+				gameObject.transform.Find("Pause Menu").gameObject.SetActive (true);
 				open = true;
 				Time.timeScale = 0;
 				Cursor.visible = true;
-				Debug.Log (open);
 				GameObject.FindGameObjectsWithTag ("Player")[0].GetComponent<FirstPersonInteractor> ().enabled = false;
 			} else {
-				open = false;
-				gameObject.transform.GetChild(0).gameObject.SetActive (false);
-				gameObject.transform.GetChild(1).gameObject.SetActive (false);
-				gameObject.transform.GetChild(2).gameObject.SetActive (false);
-				Time.timeScale = 1;
-				Cursor.visible = false;
-				GameObject.FindGameObjectsWithTag ("Player")[0].GetComponent<FirstPersonInteractor> ().enabled = true;
+				resume ();
 
 			}
 		}
@@ -41,33 +34,54 @@ public class PauseMenu : MonoBehaviour {
 	}
 
 	public void resume(){
-		gameObject.transform.GetChild(0).gameObject.SetActive (false);
+		open = false;
+		gameObject.transform.Find("Pause Menu").gameObject.SetActive (false);
+		gameObject.transform.Find("Options Menu").gameObject.SetActive (false);
+		gameObject.transform.Find("Keybindings").gameObject.SetActive (false);
 		Time.timeScale = 1;
 		Cursor.visible = false;
-		Debug.Log("Pressed esc, unpaused");
 		GameObject.FindGameObjectsWithTag ("Player")[0].GetComponent<FirstPersonInteractor> ().enabled = true;
 
 	}
 
 	public void showKeybindings(){
-		gameObject.transform.GetChild(0).gameObject.SetActive (false);
-		gameObject.transform.GetChild(2).gameObject.SetActive (true);
+		gameObject.transform.Find("Pause Menu").gameObject.SetActive (false);
+		gameObject.transform.Find("Keybindings").gameObject.SetActive (true);
+	}
+
+	public void hideShowInventoryUI(){
+		if (showInventoryUI) {
+			showInventoryUI = false;
+			gameObject.transform.Find("Slot1").gameObject.SetActive (false);
+			gameObject.transform.Find("Slot2").gameObject.SetActive (false);
+			gameObject.transform.Find("Slot3").gameObject.SetActive (false);
+			gameObject.transform.Find("Slot4").gameObject.SetActive (false);
+		} else {
+			showInventoryUI = true;
+			gameObject.transform.Find("Slot1").gameObject.SetActive (true);
+			gameObject.transform.Find("Slot2").gameObject.SetActive (true);
+			gameObject.transform.Find("Slot3").gameObject.SetActive (true);
+			gameObject.transform.Find("Slot4").gameObject.SetActive (true);
+
+		}
+
 	}
 
 	public void options(){
-		gameObject.transform.GetChild(0).gameObject.SetActive (false);
-		gameObject.transform.GetChild(1).gameObject.SetActive (true);
+		gameObject.transform.Find("Pause Menu").gameObject.SetActive (false);
+		gameObject.transform.Find("Options Menu").gameObject.SetActive (true);
 	}
 
 	public void quit(){
-		Debug.Log ("is this quitting?");
 		Application.Quit();
 	}
 
 	public void backToMainMenu(){
-		gameObject.transform.GetChild(0).gameObject.SetActive (true);
-		gameObject.transform.GetChild(1).gameObject.SetActive (false);
-		gameObject.transform.GetChild(2).gameObject.SetActive (false);
+		gameObject.transform.Find("Pause Menu").gameObject.SetActive (true);
+		gameObject.transform.Find("Options Menu").gameObject.SetActive (false);
+		gameObject.transform.Find("Keybindings").gameObject.SetActive (false);
 	}
+
+
 
 }
