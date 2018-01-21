146ce564543579c1055f7c54f2d586591e203cdf
Huiji (Chelsea) Ying <yingh@carleton.edu>
2018-01-20 23:38:56 -0600

    Create journal entry button prefab to allow dynamic scroll list item addition to UI reflecting changes in journal.

--- Project/Assets/Prairie/Framework/Script/Journal/JournalGui.cs
+++ Project/Assets/Prairie/Framework/Script/Journal/JournalGui.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class JournalGui : MonoBehaviour {
 	private bool active = false;
 	private bool isJournalOpen = false;
+	private int startingEntry = 0;
+
+	public Transform contentPanel;
+	public GameObject prefab;
 
 	// Name of the journal object in game scene.
 	private const string JOURNAL = "Journal";
@@ -35,6 +39,7 @@ public class JournalGui : MonoBehaviour {
 
 
 	public void openJournal(){
+		AddButtons ();
 		gameObject.transform.Find(JOURNAL).gameObject.SetActive (true);
 	}
 
@@ -44,5 +49,22 @@ public class JournalGui : MonoBehaviour {
 
 	public bool isOpen() {
 		return isJournalOpen;
+	}
+
+	private void AddButtons()
+	{
+		List<JournalEntry> entries = gameObject.GetComponentInParent<Journal> ().journal;
+		for (int i = startingEntry; i < entries.Count; i++) 
+		{
+			JournalEntry e = entries[i];
+			GameObject newButton = (GameObject)GameObject.Instantiate(prefab);
+
+			newButton.transform.SetParent(contentPanel);
+
+			JournalTitleGuiButton jtgb = newButton.GetComponent<JournalTitleGuiButton>();
+			jtgb.Setup(e);
+
+			startingEntry += 1;
+		}
 	}
 }
---
+++ Project/Assets/Prairie/Framework/Script/Journal/JournalTitleGuiButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class JournalTitleGuiButton : MonoBehaviour
+{
+
+	public Button buttonComponent;
+	public Text title;
+
+	private JournalEntry entry;
+
+	// Use this for initialization
+	void Start () 
+	{
+		buttonComponent.onClick.AddListener (HandleClick);
+	}
+
+	public void Setup(JournalEntry currentEntry) 
+	{
+		entry = currentEntry;
+		title.text = entry.title;
+	}
+
+	public void HandleClick() 
+	{
+	}
+
+}
