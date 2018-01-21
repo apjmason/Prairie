a46ff38b1702f286ce1d9398ea091f74954471bb
Huiji (Chelsea) Ying <yingh@carleton.edu>
2018-01-21 02:01:45 -0600

    Link Journal UI and backend script part 1: item name and annotation text content.

--- Project/Assets/Prairie/Framework/Script/Journal/JournalTitleGuiButton.cs
+++ Project/Assets/Prairie/Framework/Script/Journal/JournalTitleGuiButton.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+using System.Collections;
+using System.IO;
 
 public class JournalTitleGuiButton : MonoBehaviour
 {
-
 	public Button buttonComponent;
 	public Text title;
+
+	// Sync with the UI element names in game scene.
+	private const string NAME = "ItemName";
+	private const string INFO = "Info";
+	private const string IMAGE = "ItemPicture";
 
 	private JournalEntry entry;
 
@@ -24,6 +29,23 @@ public class JournalTitleGuiButton : MonoBehaviour
 
 	public void HandleClick() 
 	{
+		DisplayName (entry);
+		DisplayInfo (entry);
+//		DisplayImage (entry);
 	}
 
+	private void DisplayName(JournalEntry e) {
+		Text name = GameObject.Find(NAME).GetComponentInChildren<Text>();
+		name.text = entry.title;
+	}
+
+	private void DisplayInfo(JournalEntry e) {
+		Text info = GameObject.Find(INFO).GetComponentInChildren<Text>();
+		info.text = "";
+		foreach (string c in e.content) {
+			info.text += "- ";
+			info.text += c;
+			info.text += '\n';
+		}
+	}
 }
